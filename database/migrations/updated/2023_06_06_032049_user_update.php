ad90aa93a389753655845d110073c2e1bba53e96
realdeveloper007 <diwakarsharma603@gmail.com>
2023-06-06 22:14:28 +0530

    Fake data inserted into the user table with Factory & Seed command

--- database/migrations/updated/2023_06_06_032049_user_update.php
+++ database/migrations/updated/2023_06_06_032049_user_update.php
@@ -15,7 +15,7 @@ class UserUpdate extends Migration
     {
         //
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('name')->change();
+            $table->string('name')->change();
         });
     }
 
