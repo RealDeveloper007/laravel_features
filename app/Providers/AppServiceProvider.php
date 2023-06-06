aeeabef38ab0a196a928eafe16074e9ac4efaa7f
realdeveloper007 <diwakarsharma603@gmail.com>
2023-06-06 23:34:47 +0530

    View Share use for data pass on layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        $User = \App\Models\User::first();
+        View::share(compact('User'));
     }
 }
